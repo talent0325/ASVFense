b43615f735832c7c3d9f23961fe5cca1f3271a70
Talent <1197429202@qq.com>
2025-09-26 15:01:09 +0800

    v 0.2.1 add backend

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 进入 /frontend 在终端执行 ```npx next dev```
+
+进入 /backend  终端执行    ```python -m flask run```
--- backend/app.py
+++ backend/app.py
@@ -4,14 +4,16 @@ from config import Config
 from routes import api
 
 
-def create_app():
+def create_app():  # 负责创建 Flask 应用实例
     app = Flask(__name__)
-    app.config.from_object(Config)
+    app.config.from_object(Config)   #  从 config.py 里加载配置（比如 DEBUG、数据库 URL 等）
 
-    # 初始化CORS
-    CORS(app)
+    # 初始化CORS  允许跨域请求（前端运行在不同端口，比如 React 在 3000，Flask 在 5000）
+    CORS(app)   
 
     # 注册蓝图
+    # fetch('http://localhost:5000/api/record') 调用的不是文件夹，而是 蓝图里的路由
+    # 确保 routes/api.py 里有 /record 的路由，否则 fetch 会 404
     app.register_blueprint(api)
 
     return app
